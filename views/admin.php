24e2a04923c23ee64e09814e0003e06c6886d1e9
BaptisteQnt <baptiste.qnt@outlook.com>
2022-12-15 15:27:43 +0100

    Update admin.php

--- views/admin.php
+++ views/admin.php
@@ -1,7 +1,7 @@
 <?php 
 // Permet d'identifier l'administrateur du site et de faire apparaitre sur la page les fonctionnalité et les donnees dont il a besoin.
-// if(!empty($_SESSION)){ 
-if($_SESSION['id_roles'] == "1" ){ ?>
+if(!empty($_SESSION)){ 
+if($_SESSION['id_roles'] == "1"){ ?>
     <div class="mt-5 container">
         <h1>Page administrateur du site 1Experience</h1>
         <div class="container mt-5">
@@ -21,7 +21,19 @@ if($_SESSION['id_roles'] == "1" ){ ?>
         </div>
     </div>
 <!-- Permet d'afficher au autre utilisateur sur cette page un message d'erreur car il n'ont pas acces au donnees administrateur -->
-<?php } else { ?>
+<?php } if($_SESSION['id_roles'] == "2"){ ?>
+    <div class="mt-5 container">
+    <h1 class="text-center mt-5 text-danger">Vous n'êtes pas autorisé a entrer sur cette page.</h1>
+    <div>
+        <img src="views/asset/img/2762862.jpg" class="img-fluid" alt="...">
+    </div>
+    <h4 class="mt-5">Vous vous êtes perdu sur le site et êtes tomber sur cette page ? Ce n'est pas grave vous allez être redirigé sur l'acceuil automatiquement.</h4>
+    <p class="mt-4 ">Cette page est réserver aux administrateur du site. Elle sert nottament a receuillir les demande de contacte faite par d'autre utilisateur</p>
+    <div class="mt-4">
+        <small class="text-secondary">Si la redirection ne marche pas vous pourrez toujours appuyer sur le nom du site dans la barre de naviguation.</small>
+    </div>
+</div>
+<?php }} else { ?>
 <div class="mt-5 container">
     <h1 class="text-center mt-5 text-danger">Vous n'êtes pas autorisé a entrer sur cette page.</h1>
     <div>
