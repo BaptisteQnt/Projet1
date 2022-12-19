eb0678f137416afc0108ad88ad46cdfe002f2f7a
BaptisteQnt <baptiste.qnt@outlook.com>
2022-12-19 14:18:02 +0100

    information page update

--- views/information.php
+++ views/information.php
@@ -1,43 +1,30 @@
 <div class="d-flex bd-highlight">
-    <div class="p-2 flex-fill bd-highlight border">
-        <div class="">
-            <div class="d-flex flex-column ">
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none" href="#">Présentation du projets</a></div>
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none hovermoncss" href="#">Comment fonctionne ce projets</a></div>
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none hovermoncss" href="#">En savoir plus sur nous</a></div>
-            </div>
-            <div class="d-flex flex-column bd-highlight">
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none hovermoncss" href="#">Des conseils</a></div>
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none hovermoncss" href="#">Règle importante</a></div>
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none hovermoncss" href="#">Plus d'info</a></div>
-                <div class=" p-4 bd-highlight border-top border-bottom"><a class="text-danger text-decoration-none hovermoncss" href="index.php?contact">Nous contacter</a></div>
-            </div>
+    <div class="p-2 flex-fill bd-highlight ms-4" >
+        <div class="container-fluid mt-5">
+            <h3 class="text-danger fs-3 fw-bold mb-3">Présentation du projet</h3>
+            <p class="mt-5 text-start text-wrap fs-5 ms-4">1Experience est un projet se basant sur le système de stage en immersion au sein d'une entreprise. 
+                Car d'après notre expérience, les emplois sont plus facile a obtenir après un stage en entreprise de plusieurs semaine. 
+                Ainsi le recruteur est rassurer et peut pourvoir un poste avec enthousiasme.</p>
         </div>
-    </div>
-    <div class="p-2 flex-fill bd-highlight w-75 ms-4" >
-        <div class="mt-5">
-            <h3 class="mb-3">Présentation du projet</h3>
-            <p class="ms-4">lorem ispum lorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispum</p>
+        <div class="container-fluid mt-5">
+            <h3 class="text-danger fs-3 fw-bold mb-3">Comment fonctionne ce projet</h3>
+            <p class="fs-5 mt-5 text-start text-wrap ms-4">Le principe fonctionne sur un echange de bon procéder entre les entreprise, cette outils en ligne et les personnes prète a effectuer une période d'immersion.
+                L'entreprise s'inscrit via un formulaire, remplie un tableau de bord pour définir ces besoins et apparaitra ainsi sur la carte de recherche.
+            </p>
         </div>
-        <div class="mt-5">
-            <h3 class="mb-3">Comment fonctionne ce projet</h3>
-            <p class="ms-4">lorem ispum lorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispum</p>
+        <div class="container-fluid mt-5">
+            <h3 class="mb-3 text-danger fs-3 fw-bold">Comment nous contacter en cas de problème</h3>
+            <p class="fs-5 mt-5 text-start text-wrap ms-4">Pour nous contacter, Aller dans la partie entreprise dans la barre de navigation et cliquez sur nous contacter. 
+                Vous aurez un formulaire à remplire et a nous exprimer votre demande ou votre problème. Nous essayrons de vous y répondre le tôt possible. 
+                Et vous pouvez aussi nous contacter sur nos réseaux.
+            </p>
         </div>
-        <div class="mt-5">
-            <h3 class="mb-3">En savoir plus sur nous</h3>
-            <p class="ms-4">lorem ispum lorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispum</p>
-        </div>
-        <div class="mt-5">
-            <h3 class="mb-3">Règle importante</h3>
-            <p class="ms-4">lorem ispum lorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem 
-            ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispumlorem ispum</p>
+        <div class="container-fluid mt-5">
+            <h3 class="mb-3 text-danger fs-3 fw-bold">Règle importante</h3>
+            <p class="fs-5 mt-5 text-start text-wrap ms-4">En utilisant cette outils, nous attendons de votre part a être respectueux envers les personnes que vous rencontrerez, les locaux dans lesquelles vous serez acceuillie. 
+                Le respect de l'activité et des personnes est primordiale pour utiliser cette outils a sont 100%. Ainsi un encadrement est requis, 
+                pour ce faire vous pourrez passer par pole emplois, votre mission locale et dispositif locaux. 
+            </p>
         </div>
     </div>
 </div>
